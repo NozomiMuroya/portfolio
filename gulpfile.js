88ac2fc3b994e74af2d6e9795909389727222aab
Nojiniy <nonkit3418@yahoo.co.jp>
2019-06-30 11:34:21 +0900

    サーバータスク修正

--- gulpfile.js
+++ gulpfile.js
@@ -27,3 +27,15 @@ function watchFiles() {
 const build = parallel(watchFiles, css);
 
 exports.default = build;
+
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+
+gulp.task('webserver', function () {
+    gulp.src('dist') // 公開したい静的ファイルを配置したディレクトリを指定する
+        .pipe(webserver({
+            host: 'localhost',
+            port: 8000,
+            livereload: true
+        }));
+});
